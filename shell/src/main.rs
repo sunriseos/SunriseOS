7a4bbe1f7365cc288acacdd1aadf3ab41f48dc7c
Orycterope <tvermeilh@gmail.com>
2018-11-20 20:14:32 +0000

    Shell command for testing multi-threading

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -26,6 +26,8 @@ use core::fmt::Write;
 use alloc::vec::Vec;
 use logger::Loggers;
 use byteorder::{ByteOrder, LE};
+use alloc::prelude::*;
+use syscalls::{create_thread, sleep_thread, exit_thread};
 
 static mut VBE_LOGGER: VBELogger = VBELogger;
 
@@ -43,6 +45,7 @@ pub fn main() {
         match &*ps2::get_next_line() {
             "gif3" => show_gif(&mut *FRAMEBUFFER.lock(), &LOUIS3[..]),
             "gif4" => show_gif(&mut *FRAMEBUFFER.lock(), &LOUIS4[..]),
+            "test_threads" => test_threads(),
             "connect" => {
                 let handle = sm::IUserInterface::raw_new().unwrap().get_service(LE::read_u64(b"vi:\0\0\0\0\0"));
                 writeln!(&mut VBELogger, "Got handle {:?}", handle);
@@ -54,6 +57,7 @@ pub fn main() {
                 writeln!(&mut VBELogger, "exit: Exit this process");
                 writeln!(&mut VBELogger, "gif3: Print the KFS-3 meme");
                 writeln!(&mut VBELogger, "gif4: Print the KFS-4 meme");
+                writeln!(&mut VBELogger, "test_threads: Run threads that concurrently print As and Bs");
             }
             _ => { writeln!(&mut VBELogger, "Unknown command"); }
         }
@@ -96,6 +100,37 @@ fn show_gif(fb: &mut Framebuffer, louis: &[u8]) {
             }
         }
     }
+}
+
+fn test_threads() {
+
+    fn thread_a() {
+        for _ in 0..10 {
+            writeln!(&mut VBELogger, "A");
+            sleep_thread(0);
+        }
+    }
+
+    fn thread_b() -> ! {
+        for _ in 0..10 {
+            writeln!(&mut VBELogger, "B");
+            sleep_thread(0);
+        }
+        exit_thread()
+    }
+
+    const THREAD_STACK_SIZE: usize = 0x2000;
+
+    let stack = Box::new([0u8; THREAD_STACK_SIZE]);
+    let sp = (Box::into_raw(stack) as *const u8).wrapping_offset(THREAD_STACK_SIZE as isize);
+    let ip = thread_b;
+    let thread_handle = create_thread(ip, 0, sp, 0, 0)
+        .expect("svcCreateThread returned an error");
+    thread_handle.start()
+        .expect("svcStartThread returned an error");
+
+    // thread is running b, run a meanwhile
+    thread_a();
 }
 
 static LOUIS3: &'static [u8; 1318100] = include_bytes!("../img/meme3.gif");
