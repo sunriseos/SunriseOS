65b061c76c61d410df35b1bbd6a61ff3852715f1
Orycterope <tvermeilh@gmail.com>
2018-11-27 20:18:31 +0000

    Paging fix, error handling

--- kernel/src/interrupts/syscalls.rs
+++ kernel/src/interrupts/syscalls.rs
@@ -45,7 +45,7 @@ fn map_framebuffer() -> Result<(usize, usize, usize, usize), UserspaceError> {
     //let framebuffer_vaddr = memory.find_virtual_space::<UserLand>(frame_buffer_phys_region.size())?;
     // todo make user provide the address
     let framebuffer_vaddr = VirtualAddress(0x80000000);
-    memory.map_phys_region_to(frame_buffer_phys_region, framebuffer_vaddr, MappingFlags::u_rw());
+    memory.map_phys_region_to(frame_buffer_phys_region, framebuffer_vaddr, MappingFlags::u_rw())?;
 
     let addr = framebuffer_vaddr.0;
     let width = tag.framebuffer_dimensions().0 as usize;
