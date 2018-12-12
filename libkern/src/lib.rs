4ab7f686fc82b9fe1002aa46b820dd669bcd8c1c
roblabla <unfiltered@roblab.la>
2018-12-12 19:04:50 +0000

    Fully document libkern.

--- libkern/src/lib.rs
+++ libkern/src/lib.rs
@@ -11,67 +11,121 @@ extern crate bitflags;
 extern crate lazy_static;
 
 pub mod error;
-
+ 
 enum_with_val! {
+    /// The type of this memory area.
+    ///
+    /// May be used to figure out how the memory area was created.
     #[derive(Default, Clone, Copy, PartialEq, Eq)]
     pub struct MemoryType(u32) {
+        /// An unmapped memory region. Reading will lead to a data abort.
         Unmapped = 0,
+        /// Mapped by kernel capability parsing in `create_process` syscall.
         Io = 0x00002001,
+        /// Mapped by kernel capability parsing in `create_process` syscall.
         Normal = 0x00042002,
+        /// Mapped during create_process
         CodeStatic = 0x00DC7E03,
         // 1.0.0-3.1.0 0x01FEBD04
         // 4.0.0+
+        /// Transition from MemoryType::CodeStatic performed by
+        /// `set_process_memory_permission` syscall.
         CodeMutable = 0x03FEBD04,
         // 1.0.0-3.1.0 0x017EBD05
+        /// Mapped using `set_heap_size` syscall.
         Heap = 0x037EBD05,
+        /// Mapped using `map_shared_memory` syscall.
         SharedMemory = 0x00402006,
+        /// Mapped using `map_memory` syscall.
+        // 1.0.0 only
         Alias = 0x00482907,
+        /// Mapped using `map_process_code_memory` syscall.
         ModuleCodeStatic = 0x00DD7E08,
         // 1.0.0-3.1.0: 0x01FFBD09
+        /// Transition from MemoryType::ModuleCodeStatic by
+        /// `set_process_memory_permission` syscall.
         ModuleCodeMutable = 0x03FFBD09,
+        /// IPC buffers with descriptor flags=0.
         IpcBuffer0 = 0x005C3C0A,
+        /// Mapped using `map_memory` syscall.
         Stack = 0x005C3C0B,
+        /// Mapped during `create_thread` syscall.
         ThreadLocal = 0x0040200C,
+        /// Mapped using `map_transfer_memory` syscall when the owning process
+        /// has `perm = 0`.
         TransferMemoryIsolated = 0x015C3C0D,
+        /// Mapped using `map_transfer_memory` syscall when the owning process
+        /// has `perm != 0`.
         TransferMemory = 0x005C380E,
+        /// Mapped using `map_process_memory` syscall.
         ProcessMemory = 0x0040380F,
+        /// Reserved memory area, used internally by kernel, should not be
+        /// observable.
         Reserved = 0x00000010,
+        /// IPC buffers with descriptor flags=1.
         IpcBuffer1 = 0x005C3811,
+        /// IPC buffers with descriptor flags=3.
         IpcBuffer3 = 0x004C2812,
+        /// Mapped in kernel during `create_thread`. Should not be observable.
         KernelStack = 0x00002013,
+        /// Mapped using `control_code_memory` syscall.
         CodeReadOnly = 0x00402214,
+        /// Mapped using `control_code_memory` syscall.
         CodeWritable = 0x00402015,
     }
 }
 
 bitflags! {
+    /// Low-level attributes of a memory mapping.
     #[derive(Default)]
     pub struct MemoryAttributes : u32 {
+        /// Is mapped in more than one area.
         const BORROWED = 1 << 0;
+        /// Is mapped through an IPC request.
         const IPC_MAPPED = 1 << 0;
+        /// Is a device mapping.
         const DEVICE_MAPPED = 1 << 2;
+        /// Is caching disabled in the MMU.
         const UNCACHED = 1 << 3;
     }
 }
 
 bitflags! {
+    /// Memory permissions of a memory area.
     #[derive(Default)]
     pub struct MemoryPermissions : u32 {
+        /// The area is readable.
         const READABLE = 1 << 0;
+        /// The area is writable.
         const WRITABLE = 1 << 1;
+        /// The area is executable.
         const EXECUTABLE = 1 << 2;
     }
 }
 
+/// The structure returned by the `query_memory` structure.
 #[repr(C)]
 #[derive(Debug, Default)]
 pub struct MemoryInfo {
+    /// The base address of this memory region.
+    ///
+    /// This is not the same as the address passed in query_memory. It will
+    /// always be lower or equal to it, and will always be page-aligned. It
+    /// points to the beggining of the mapping that address falls into.
     pub baseaddr: usize,
+    /// The size of this memory region, from the base address.
     pub size: usize,
+    /// The type of this mapping.
+    ///
+    /// Used to figure out how this mapping was created.
     pub memtype: MemoryType,
+    /// The attributes of this mapping.
     pub memattr: MemoryAttributes,
+    /// The permissions of this mapping.
     pub perms: MemoryPermissions,
+    /// Counts how many IPC service requests have an IPC buffer in this mapping.
     pub ipc_ref_count: u32,
+    /// Unknown.
     pub device_ref_count: u32,
 }
 
@@ -83,12 +137,21 @@ macro_rules! syscalls {
         $($name:ident = $id:expr),* $(,)*
     ) => {
         pub mod $byid {
+            //! Syscall numbers
+            //!
+            //! This module contains all the syscalls number <=> string
+            //! associations.
+
+            #![allow(missing_docs)]
+
             $(
                 #[allow(non_upper_case_globals)]
                 pub const $name: usize = $id;
             )*
         }
         lazy_static! {
+            /// A table associating a syscall name string for every syscall
+            /// number.
             pub static ref $byname: [&'static str; $maxid] = {
                 let mut arr = ["Unknown"; $maxid];
                 $(arr[$id] = stringify!($name);)*
