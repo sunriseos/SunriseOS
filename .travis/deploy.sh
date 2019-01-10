68d0c2c33de478e7bb9f7ee99d70964deaa2c559
roblabla <unfiltered@roblab.la>
2019-01-10 23:56:05 +0000

    travis: Set the permissions of the deploy_key to 600
    SSH complains loudly and ignores the given key if the permissions are
    too open. Let's just make ssh happy I guess...

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 # Decrypt and setup the deploy key.
 openssl aes-256-cbc -K $encrypted_a3c22127997a_key -iv $encrypted_a3c22127997a_iv -in .travis/github_deploy_key.enc -out .travis/github_deploy_key -d
 eval "$(ssh-agent)"
+chmod 600 .travis/github_deploy_key
 ssh-add .travis/github_deploy_key
 
 # Deploy!
