5311bf441e4a9596349d0df8c8a78a74dd13f39c
Orycterope <tvermeilh@gmail.com>
2019-03-11 17:52:44 +0000

    shell: Adding old kfs-1 and kfs-2 memes
    Moving them from the grub folder to shell/img/,
    and converting them to GIF format.

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -45,8 +45,10 @@ fn main() {
     let mut terminal = Terminal::new(WindowSize::FontLines(-1, false)).unwrap();
     loop {
         match &*ps2::get_next_line(&mut terminal) {
-            "gif3" => show_gif(&LOUIS3[..]),
-            "gif4" => show_gif(&LOUIS4[..]),
+            "meme1" => show_gif(&LOUIS1[..]),
+            "meme2" => show_gif(&LOUIS2[..]),
+            "meme3" => show_gif(&LOUIS3[..]),
+            "meme4" => show_gif(&LOUIS4[..]),
             "test_threads" => terminal = test_threads(terminal),
             "test_divide_by_zero" => test_divide_by_zero(),
             "test_page_fault" => test_page_fault(),
@@ -59,8 +61,10 @@ fn main() {
             "help" => {
                 let _ = writeln!(&mut terminal, "COMMANDS:");
                 let _ = writeln!(&mut terminal, "exit: Exit this process");
-                let _ = writeln!(&mut terminal, "gif3: Print the KFS-3 meme");
-                let _ = writeln!(&mut terminal, "gif4: Print the KFS-4 meme");
+                let _ = writeln!(&mut terminal, "meme1: Display the KFS-1 meme");
+                let _ = writeln!(&mut terminal, "meme2: Display the KFS-2 meme");
+                let _ = writeln!(&mut terminal, "meme3: Display the KFS-3 meme");
+                let _ = writeln!(&mut terminal, "meme4: Display the KFS-4 meme");
                 let _ = writeln!(&mut terminal, "test_threads: Run threads that concurrently print As and Bs");
                 let _ = writeln!(&mut terminal, "test_divide_by_zero: Check exception handling by throwing a divide by zero");
                 let _ = writeln!(&mut terminal, "test_page_fault: Check exception handling by throwing a page_fault");
@@ -197,6 +201,10 @@ fn test_page_fault() {
     let _res = unsafe { *ptr };
 }
 
+/// Meme for KFS1
+static LOUIS1: &'static [u8; 89915] = include_bytes!("../img/meme1.gif");
+/// Meme for KFS2
+static LOUIS2: &'static [u8; 93818] = include_bytes!("../img/meme2.gif");
 /// Meme for KFS3
 static LOUIS3: &'static [u8; 1318100] = include_bytes!("../img/meme3.gif");
 /// Meme for KFS4
