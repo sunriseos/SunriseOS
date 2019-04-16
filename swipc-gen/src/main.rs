25eb028a338ea368da08c60508833546a3a50bdf
roblabla <unfiltered@roblab.la>
2019-04-16 00:08:08 +0000

    SwIPC-Gen: Better binary to test.
    The SwIPC-Gen binary now has a better interface to play around with the
    code generated. Furthermore, to make using it simple, a Makefile rule,
    swipc-gen, was added, that will build and run the tool with the required
    arguments.

--- swipc-gen/src/main.rs
+++ swipc-gen/src/main.rs
@@ -4,16 +4,34 @@
 
 #![feature(box_patterns)]
 
-use std::path::Path;
+use std::path::PathBuf;
 use std::fs;
 
 mod gen_rust_code;
 
 use gen_rust_code::generate_ipc;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// Path to the module to generate code under, as a ::-delimited path.
+    prefix: String,
+    /// Name of the module to create.
+    mod_name: String,
+    /// SwIPC files to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
 
 fn main() {
-    for arg in std::env::args().skip(1) {
-        let id_file = fs::read_to_string(Path::new(&arg)).unwrap();
-        println!("{}", generate_ipc(&id_file, "libuser".to_string(), "vi".to_string(), "libuser".to_string()));
+    let opt = Opt::from_args();
+
+    for file in opt.files {
+        let id_file = fs::read_to_string(file).unwrap();
+        // Get crate name from prefix. In the future, those might need to be
+        // different, but let's keep the CLI simple for now.
+        let crate_name = opt.prefix.split("::").nth(0).unwrap().to_string();
+        println!("{}", generate_ipc(&id_file, opt.prefix.clone(), opt.mod_name.clone(), crate_name));
     }
 }
