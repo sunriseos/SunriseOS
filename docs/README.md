8b100709b327da6dff9064dbc50537fd653fe8bb
roblabla <unfiltered@roblab.la>
2019-07-17 17:15:28 +0000

    Fix wrong README link

--- docs/README.md
+++ docs/README.md
@@ -2,4 +2,4 @@
 
 SunriseOS is a reimplementation of the Horizon/NX kernel (which powers the Nintendo Switch) along with a custom userspace of our own design (but heavily inspired by the Horizon/NX userspace), written entirely in Rust with no dependencies on C/C++.
 
-In the various submodules, you'll find various documentations about the overall architecture, along with various development processes that are used.
+In the various modules, you'll find various documentations about the overall architecture, along with various development processes that are used.
--- docs/src/main.rs
+++ docs/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(external_doc)]
-#![doc(include = "../../README.md")]
-
+#![doc(include = "../README.md")]
+ 
 fn main() {}
 
 pub mod building {
@@ -9,3 +9,13 @@ pub mod building {
 pub mod updating_rust_version {
     #![doc(include = "../UPDATE_RUST.md")]
 }
+
+// TODO: Add design goals documentation.
+// BODY: Add documentation about the design goals of SunriseOS.
+// BODY: This should include:
+// BODY:
+// BODY: - Simple to build
+// BODY:   - Requires only a rust toolchain that can be installed straight from rustup
+// BODY: - Simple to install
+// BODY: - Every piece should be documented such that an outsider may understand what we're up to.
+// BODY: - Only support modern hardware
