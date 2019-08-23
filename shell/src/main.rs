2ee1e48e7fc82bca3bc97feddd4690da17a4c49a
roblabla <unfiltered@roblab.la>
2019-08-23 13:41:16 +0000

    Add some very simple login system

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -19,6 +19,7 @@
 #![deny(intra_doc_link_resolution_failure)]
 
 use gif;
+#[macro_use]
 extern crate alloc;
 #[macro_use]
 extern crate log;
@@ -37,6 +38,7 @@ use crate::libuser::window::{Window, Color};
 use crate::libuser::terminal::{Terminal, WindowSize};
 use crate::libuser::threads::Thread;
 use crate::libuser::error::{Error, FileSystemError};
+use crate::libuser::syscalls;
 
 use core::fmt::Write;
 use alloc::vec::Vec;
@@ -50,6 +52,55 @@ lazy_static! {
     static ref CURRENT_WORK_DIRECTORY: Mutex<String> = Mutex::new(String::from("/"));
 }
 
+/// Asks the user to login repeatedly. Returns with an error if the /etc/passwd
+/// file is invalid or doesn't exist.
+fn login(mut terminal: &mut Terminal, filesystem: &IFileSystemProxy) -> Result<(), Error> {
+    let mut ipc_path = [0x0; 0x300];
+    ipc_path[..b"/etc/passwd".len()].copy_from_slice(b"/etc/passwd");
+
+    let file: IFileProxy = filesystem.open_file(1, &ipc_path)?;
+    let size = file.get_size().expect("get_size to work");
+    let mut data = vec![0; size as usize];
+    let read_count = file.read(0, 0, size, &mut data).expect("Read to work");
+    data.resize(read_count as usize, 0);
+    let data = match String::from_utf8(data) {
+        Ok(data) => data,
+        Err(_err) => {
+            warn!("Invalid FSTAB: non-utf8 data found");
+            return Ok(())
+        }
+    };
+
+    // Login
+    loop {
+        let _ = write!(&mut terminal, "Login: ");
+        let _ = terminal.draw();
+        let username = ps2::get_next_line(&mut terminal, true);
+        let username = username.trim_end_matches('\n');
+
+        let _ = writeln!(&mut terminal, "Password: ");
+        let password = ps2::get_next_line(&mut terminal, false);
+        let password = password.trim_end_matches('\n');
+
+        let hash = sha1::Sha1::from(&password).digest().bytes();
+
+        for item in data.split('\n') {
+            let mut it = item.split(' ');
+            if let (Some(item_username), Some(item_hash)) = (it.next(), it.next()) {
+                if let Ok(item_hash) = hex::decode(item_hash) {
+                    if username == item_username && hash[..] == item_hash[..] {
+                        let _ = writeln!(&mut terminal, "Login Success!");
+                        return Ok(());
+                    }
+                }
+            }
+        }
+
+        let _ = writeln!(&mut terminal, "Invalid login or password");
+        let _ = syscalls::sleep_thread(1 * 1000 * 1000 * 1000);
+    }
+}
+
 fn main() {
     let mut terminal = Terminal::new(WindowSize::FontLines(-1, false)).unwrap();
 
@@ -58,8 +109,12 @@ fn main() {
 
     cat(&mut terminal, &filesystem, "/etc/motd").unwrap();
 
+    if let Err(err) = login(&mut terminal, &filesystem) {
+        error!("Error while setting up login: {:?}", err);
+    }
+
     loop {
-        let line = ps2::get_next_line(&mut terminal);
+        let line = ps2::get_next_line(&mut terminal, true);
         let mut arguments = line.split_whitespace();
         let command_opt = arguments.next();
 
