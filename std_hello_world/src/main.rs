6b21406176b4f26a285676d80935d74daa5afb53
Thomas Guillemard <me@thog.eu>
2019-09-10 15:06:04 +0000

    add some more std tests

--- std_hello_world/src/main.rs
+++ std_hello_world/src/main.rs
@@ -3,10 +3,35 @@ extern crate sunrise_libuser as libuser;
 
 use std::time::SystemTime;
 use std::fs::OpenOptions;
+use std::io;
 use std::io::Write;
 use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
+
+use std::fs::{self, DirEntry};
+use std::path::Path;
+
+// one possible implementation of walking a directory only visiting files
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+    //println!("{}", dir.is_dir());
+    //if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    //}
+    Ok(())
+}
+
+fn print_entry(entry: &DirEntry) {
+    println!("{:?}", entry);
+}
 
 fn main() {
     println!("Hello from main");
@@ -27,6 +52,9 @@ fn main() {
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
+
+    println!("{:?}", fs::metadata(Path::new("system:/etc")).map(|m| m.is_dir()));
+    visit_dirs(Path::new("/"), &print_entry).unwrap();
 }
 
 // TODO: Move this out of here
