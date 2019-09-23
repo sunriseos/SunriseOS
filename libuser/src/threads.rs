66e43f614d8ae16c9f0b6ad5a0da9da24a3ef7f5
Thomas Guillemard <me@thog.eu>
2019-09-23 16:17:31 +0000

    Apply roblabla suggestion about init_main_thread

--- libuser/src/threads.rs
+++ libuser/src/threads.rs
@@ -364,18 +364,17 @@ impl Drop for Thread {
     }
 }
 
+/// Initialisation of the main thread's thread local structures:
+///
+/// When a main thread starts, the kernel puts the handle of its own thread in one of its registers.
+/// The main thread should perform relocations, and then call this function, which will:
+///
+/// * put the main thread's handle in [MAIN_THREAD_CONTEXT].
+/// * save a pointer to it in its [TLS].
+/// * perform copy of `.tdata` and `.tbss` for the main thread.
 #[cfg(feature = "build-for-std-app")]
-extern {
-    /// Initialisation of the main thread's thread local structures:
-    ///
-    /// When a main thread starts, the kernel puts the handle of its own thread in one of its registers.
-    /// The main thread should perform relocations, and then call this function, which will:
-    ///
-    /// * put the main thread's handle in [MAIN_THREAD_CONTEXT].
-    /// * save a pointer to it in its [TLS].
-    /// * perform copy of `.tdata` and `.tbss` for the main thread.
-    #[no_mangle] // called from asm
-    pub fn init_main_thread(handle: ThreadHandle);
+#[no_mangle] // called from asm
+pub extern fn init_main_thread(_handle: ThreadHandle) {
 }
 
 /// Initialisation of the main thread's thread local structures:
